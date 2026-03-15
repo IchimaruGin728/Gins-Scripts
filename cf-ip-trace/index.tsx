fab53b02b8fe35df0b4f3967e1b3e3e4286204db
IchimaruGin <ichimarugin728@gmail.com>
2026-03-15 22:34:28 +0800

    fix(cf-ip-trace): fix entry point and optimize background execution

--- cf-ip-trace/index.tsx
+++ cf-ip-trace/index.tsx
@@ -1,5 +1,9 @@
-import { Script } from "scripting"
+import { Script, config } from "scripting"
 
-console.present().then(() => Script.exit())
-console.log("请将此脚本添加为桌面小组件使用")
-console.log("显示当前 IP、CF 接入节点、延迟和下载速率")
+if (config.runsInApp) {
+  console.present().then(() => Script.exit())
+  console.log("请将此脚本添加为桌面小组件使用")
+}
+
+// 统一入口
+import("./widget")
