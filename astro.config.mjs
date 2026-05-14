d9019e59ff1fbf0baa2d715841ae49826cb97d97
IchimaruGin728 <ichimarugin728@gmail.com>
2026-05-14 14:17:34 +0800

    refactor: move frontend files to frontend/ directory

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import UnoCSS from "@unocss/astro"
 import { defineConfig } from "astro/config"
 
 export default defineConfig({
+  srcDir: "./frontend/src",
+  publicDir: "./frontend/public",
   integrations: [UnoCSS()],
   output: "static",
 })
